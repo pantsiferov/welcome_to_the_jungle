67596d5ba487d430c71444cd5aafeca2030ee2b2
pantsiferov <Pavel.Antsiferov@noveogroup.com>
2020-01-14 20:31:24 +0300

    init ui-kit config

--- src/index.js
+++ src/index.js
@@ -2,20 +2,40 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
+import { ThemeProvider } from '@xstyled/styled-components'
+import { createTheme, GlobalStyle } from '@welcome-ui/core'
 import store, { history } from './store'
 import App from './AppLayout'
 
 import 'sanitize.css/sanitize.css'
 import './index.css'
 
+const options = {
+  defaultFontFamily: 'Helvetica',
+  headingFontFamily: 'Georgia',
+  colors: {
+    primary: {
+      500: '#FF0000'
+    },
+    secondary: {
+      500: '#00FF00'
+    }
+  }
+}
+// Create your theme
+const theme = createTheme(options)
+
 const target = document.querySelector('#root')
 
 render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <div>
-        <App />
-      </div>
+      <ThemeProvider theme={theme}>
+        <>
+          <GlobalStyle />
+          <App />
+        </>
+      </ThemeProvider>
     </ConnectedRouter>
   </Provider>,
   target
